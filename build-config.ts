3b789379d61dd74f3c35de9c4b5a784d40b723e3
javadbat <javadbat@gmail.com>
2025-02-01 15:36:44 +0330

    feat: add build config

---
+++ build-config.ts
@@ -0,0 +1,26 @@
+import type { ReactComponentBuildConfig, WebComponentBuildConfig } from "../../tasks/build/builder/src/types.ts";
+
+export const webComponentList: WebComponentBuildConfig[] = [
+  {
+    name: "jb-pin-input",
+    path: "./lib/jb-pin-input.ts",
+    outputPath: "./dist/jb-pin-input.js",
+    umdName: "JBPinInput",
+    external: ["jb-validation", "jb-form"],
+    globals: {
+      "jb-validation": "JBValidation",
+    },
+  },
+];
+export const reactComponentList: ReactComponentBuildConfig[] = [
+  {
+    name: "jb-pin-input-react",
+    path: "./react/lib/JBPinInput.tsx",
+    outputPath: "./react/dist/JBPinInput.js",
+    external: ["jb-pin-input", "prop-types", "react"],
+    globals: {
+      react: "React",
+      "prop-types": "PropTypes",
+    },
+  },
+];
