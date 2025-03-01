05848a46a949f29b56919897167efd734c8b242c
javadbat <javadbat@gmail.com>
2025-03-02 00:05:46 +0330

    update build config

--- build-config.ts
+++ build-config.ts
@@ -6,9 +6,11 @@ export const webComponentList: WebComponentBuildConfig[] = [
     path: "./lib/jb-pin-input.ts",
     outputPath: "./dist/jb-pin-input.js",
     umdName: "JBPinInput",
-    external: ["jb-validation", "jb-form"],
+    external: ["jb-validation", "jb-form", "jb-core"],
     globals: {
       "jb-validation": "JBValidation",
+      "jb-core":"JBCore",
+      "jb-form":"JBForm"
     },
   },
 ];
@@ -17,10 +19,13 @@ export const reactComponentList: ReactComponentBuildConfig[] = [
     name: "jb-pin-input-react",
     path: "./react/lib/JBPinInput.tsx",
     outputPath: "./react/dist/JBPinInput.js",
-    external: ["jb-pin-input", "prop-types", "react"],
+    external: ["jb-pin-input", "prop-types", "react", "jb-core", "jb-core/react"],
     globals: {
       react: "React",
       "prop-types": "PropTypes",
+      "jb-pin-input": "JBPinInput",
+      "jb-core":"JBCore",
+      "jb-core/react":"JBCoreReact"
     },
     umdName: "JBPinInputReact",
     dir: "./react"
