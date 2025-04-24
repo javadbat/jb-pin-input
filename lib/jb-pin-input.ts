23cf70ab77d74bf0c70e388d393a98ba60fd2bc5
javadbat <javadbat@gmail.com>
2025-04-24 15:39:54 +0330

    refactor:  use jb color palette

--- lib/jb-pin-input.ts
+++ lib/jb-pin-input.ts
@@ -3,6 +3,7 @@ import CSS from './jb-pin-input.scss';
 import { ValidationItem, ValidationResult, type WithValidation, ValidationHelper, ShowValidationErrorParameters } from 'jb-validation';
 import { Elements, ValidationValue } from "./types.js";
 import { type JBFormInputStandards } from 'jb-form';
+import {defineColors} from 'jb-core/theme';
 
 export * from './types.js';
 
@@ -130,6 +131,7 @@ export class JBPinInputWebComponent extends HTMLElement implements WithValidatio
       mode: 'open',
       delegatesFocus: true
     });
+    defineColors();
     const html = `<style>${CSS}</style>` + '\n' + HTML;
     const element = document.createElement('template');
     element.innerHTML = html;
